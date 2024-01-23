0d58971f93d3db0768a6cd93d8fab58424493463
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2024-01-23 22:23:35 +0530

    feat:- changing some text and use them

--- basicangular/src/app/app.component.ts
+++ basicangular/src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'basicangular';
+  title = ', lets learn Angular step by step';
 }
