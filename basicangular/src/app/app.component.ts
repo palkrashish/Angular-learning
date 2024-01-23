662c5d0b7de7860feb4e3a611cb88019a1880bc3
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2024-01-24 00:36:54 +0530

    feat:- Create module and use them as use them ☦

--- basicangular/src/app/app.component.ts
+++ basicangular/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from "./login/login.component";
+import { UserAuthModule } from './user-auth/user-auth.module';
+import { ForgetComponent } from './user-auth/forget/forget.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, LoginComponent]
+    imports: [RouterOutlet, LoginComponent, ForgetComponent]
 })
 export class AppComponent {
   title = ', lets learn Angular step by step';
--- basicangular/src/app/user-auth/user-auth.module.ts
+++ basicangular/src/app/user-auth/user-auth.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ForgetComponent } from './forget/forget.component';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    ForgetComponent
+  ],
+  exports:[ForgetComponent
+    
   ]
 })
 export class UserAuthModule { }
