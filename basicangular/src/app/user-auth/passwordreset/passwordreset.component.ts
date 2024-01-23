226a131c870a52eaa1b548a913a0f66b62c8d894
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2024-01-23 22:49:46 +0530

    feat :- learn the command how to create component and moduel

---
+++ basicangular/src/app/user-auth/passwordreset/passwordreset.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-passwordreset',
+  standalone: true,
+  imports: [],
+  templateUrl: './passwordreset.component.html',
+  styleUrl: './passwordreset.component.css'
+})
+export class PasswordresetComponent {
+
+}
